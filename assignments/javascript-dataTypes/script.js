e8c4e415b0abdd8f05cb38160d1406b29f6841ad
VleraKastrati <vlerakastrati22@gmail.com>
2024-05-27 20:10:54 +0200

    javascript-data-types

---
+++ assignments/javascript-dataTypes/script.js
@@ -0,0 +1,12 @@
+let firstName = "Vlera";
+let lastName = "Kastrati";
+let age = 23;
+let isStudent = false;
+let yearOfBirth = 2000;
+
+
+console.log(`My name is ${firstName}`);
+console.log(`My lastname is ${lastName}`);
+console.log(`I am ${age} years old`);
+console.log(`Enrolled: ${isStudent}`);
+console.log(`My year of birth is ${yearOfBirth}`);
