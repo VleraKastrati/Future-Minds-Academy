9e26a9f2fe7868bbf1e868b047d4be3f4cb16e6d
VleraKastrati <vlerakastrati22@gmail.com>
2024-06-11 22:54:30 +0200

    simple-js-challenges

---
+++ sessions/snippet-code/question.js
@@ -0,0 +1,23 @@
+let allQuestions = [
+    {
+    question: 'What does HTML stand for?',
+    answer: 'HyperText Markup Language',
+    options: [
+    'HyperText  Making Language',
+    'HyperText Markup Language',
+    'HipHop Making Language',
+    'Hinting Markup Language'
+    ]
+    },
+    {
+    question: 'Which protocol we use to secure our Web?',
+    answer: 'HTTPS',
+    options: [
+    'HTTP',
+    'TCPIP',
+    'SSH',
+    'HTTPS'
+    ]
+  }
+
+]
