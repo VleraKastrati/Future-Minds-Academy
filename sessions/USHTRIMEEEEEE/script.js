c01b73f0b20191f0fc959d5918b030daad773ae8
VleraKastrati <vlerakastrati22@gmail.com>
2024-06-25 18:12:02 +0200

    hailstone-numbers

---
+++ sessions/USHTRIMEEEEEE/script.js
@@ -0,0 +1,11 @@
+console.log (`Hello, World.`);
+console.log (`I like Pizza!`);
+
+window.alert (`It is an alert!`);
+window.alert ("I like to order Pizza!");
+window.alert (`I want a bottle of water too!`);
+
+document.getElementById("Header").textContent = "Hello";
+document.getElementById("Para").textContent = "Hello, Lel";
+
+const h2 = document.querySelector('.h2');
